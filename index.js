613149813a99699a2caae38afb4ec5b9201ca7f0
Jessica <jduesing23@gmail.com>
2024-07-20 08:38:06 -0500

    utilize express middleware to upload to existing directory

--- index.js
+++ index.js
@@ -1,11 +1,39 @@
 const express = require('express');
+const fileUpload = require('express-fileupload');
 const app = express();
 const port = 3000;
+
+//use express fileupload middleware
+app.use(fileUpload());
+
+
+//serve index page
+app.use(express.static('public'));
+
+
 
 app.get('/', (req, res) => {
     res.send("Hello World!");
 })
 
+//install express middleware at this point
+app.post('/upload', (req, res) => {
+
+    //get file set to our field named 'image
+    const {image} = req.files;
+
+    if(!image) return res.sendStatus(400);
+
+    //post to the upload directory
+    image.mv(__dirname + '/upload/' + image.name);
+
+    //console.log(req.files);
+
+    res.sendStatus(200);
+})
+
+
+//listening
 app.listen(port, () => {
     console.log(`Example app listening on ${port}`);
 })
